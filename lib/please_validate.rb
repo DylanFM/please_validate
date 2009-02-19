8ca9f21adfb2e629a90d5c68f3bfdda7d88cb115
DylanFM <dylan.fm@gmail.com>
2009-02-19 21:01:43 +1100

    Validates with w3c web service. Green

--- lib/please_validate.rb
+++ lib/please_validate.rb
@@ -1,9 +1,14 @@
+require 'net/http'
+require 'cgi'
+
 class PleaseValidate
   
   class << self
-    def file(file)
-      @file = file
-      { :status => :valid }
+    def file(file_path)
+      result = File.open(file_path, 'r') do |f|
+        Net::HTTP.start('validator.w3.org').post('/check',"fragment=#{CGI.escape(f.read)}&output=xml")
+      end
+      { :status => result['x-w3c-validator-status'].downcase.to_sym }
     end
   end
   
