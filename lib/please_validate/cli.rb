22b66c7023aff8ee120844e14d7cc16987d9cb6c
DylanFM <dylan.fm@gmail.com>
2009-03-10 00:11:43 +1100

    Some help for the cli and code for options handling (thanks newgem).

--- lib/please_validate/cli.rb
+++ lib/please_validate/cli.rb
@@ -1,3 +1,4 @@
+require 'optparse'
 require 'colored'
 
 module PleaseValidate
@@ -5,6 +6,30 @@ module PleaseValidate
     class << self
       # Execute the CLI
       def execute(stdout, arguments=[])
+        options = {}
+        mandatory_options = %w(  )
+
+        parser = OptionParser.new do |opts|
+          opts.banner = <<-BANNER.gsub(/^          /,'')
+            Please Validate does some lovely markup validation of your (X)HTML files.
+
+            Usage: #{File.basename($0)} /path/to/file [options]
+
+            Options are:
+          BANNER
+          opts.separator ""
+          # opts.on("-p", "--path=PATH", String,
+          #                   "This is a sample message.",
+          #                   "For multiple lines, add more strings.",
+          #                   "Default: ~") { |arg| options[:path] = arg }
+          opts.on("-h", "--help",
+                  "Show this help message.") { stdout.puts opts; exit }
+          opts.parse!(arguments)
+
+          if mandatory_options && mandatory_options.find { |option| options[option.to_sym].nil? }
+            stdout.puts opts; exit
+          end
+        end
         new(arguments)
       end
     end
