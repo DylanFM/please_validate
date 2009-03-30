530196048ff1f9a45d817f131405b310167e80f5
DylanFM <dylan.fm@gmail.com>
2009-03-31 00:10:34 +1100

    Adding CLI option for running in quiet mode which hides any errors for things like file not found, not valid and so on.

--- lib/please_validate/cli.rb
+++ lib/please_validate/cli.rb
@@ -6,7 +6,7 @@ module PleaseValidate
     class << self
       # Execute the CLI
       def execute(stdout, arguments=[])
-        options = {}
+        options = { :quiet => false }
         mandatory_options = %w(  )
 
         parser = OptionParser.new do |opts|
@@ -22,6 +22,8 @@ module PleaseValidate
           #                   "This is a sample message.",
           #                   "For multiple lines, add more strings.",
           #                   "Default: ~") { |arg| options[:path] = arg }
+          opts.on("-q", "--quiet",
+                  "Hide invalid file notices.") { options[:quiet] = true }
           opts.on("-h", "--help",
                   "Show this help message.") { stdout.puts opts; exit }
           opts.parse!(arguments)
@@ -30,14 +32,14 @@ module PleaseValidate
             stdout.puts opts; exit
           end
         end
-        validation = new(arguments)
+        validation = new(arguments, options)
         stdout.puts validation.msg
       end
     end
     
     # Takes the arguments, passes it to validate for validation and displays the result
-    def initialize(arguments)
-      @result = validate arguments
+    def initialize(arguments, options)
+      @result, @options = validate(arguments), options
       @msg = display
     end
     
@@ -50,25 +52,37 @@ module PleaseValidate
     # Displays the request's results
     def display
       @result.inject('') do |msg,result|
-        if result.is_a? Hash
-          msg += "#{result[:status].to_s.capitalize}: #{result[:file]}".send(result[:status] == :valid ? :on_green : :on_red)
-          if result[:status] == :invalid
-            msg += "\n#{result[:error_count]} error#{result[:error_count] == 1 ? nil:'s'}:"
-            result[:errors].each do |error|
-              msg += "\nLine #{error[:line]}, Column #{error[:col]}".red + ": #{error[:message]}"
-            end
-          end
-        elsif result.is_a? String
+        if result.is_a? String
+          next msg if options[:quiet] && result =~ /^Validation failed:/
           msg += result
+        elsif result.is_a? Hash
+          msg += message_for result
         end
-        msg += "\n\n"
-        msg
+        "#{msg}\n\n"
       end
     end
     
-    #Displays the message for the command line result
+    # Displays the message for the command line result
     def msg
       @msg
+    end
+    
+  protected
+    # Returns a hash of options passed to command line invocation
+    def options
+      @options
+    end
+    
+    # Takes one result from the validation and makes a message suitable for output
+    def message_for(result)
+      msg = "#{result[:status].to_s.capitalize}: #{result[:file]}".send(result[:status] == :valid ? :on_green : :on_red)
+      if result[:status] == :invalid
+        msg += "\n#{result[:error_count]} error#{result[:error_count] == 1 ? nil:'s'}:"
+        result[:errors].each do |error|
+          msg += "\nLine #{error[:line]}, Column #{error[:col]}".red + ": #{error[:message]}"
+        end
+      end
+      msg
     end
   end
 end
